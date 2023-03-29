b3a4f56dca1c0a71473048246471b6c821f6e03f
Domenico Cristo <84397096+domenicocristo@users.noreply.github.com>
2023-03-29 20:17:03 +0200

    Imported launch calendar API into SpaceCalendar component.

--- src/components/SpaceCalendar.js
+++ src/components/SpaceCalendar.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Navbar from './Navbar.js';
 import Footer from './Footer.js';
 import SpaceCalendarCSS from './SpaceCalendar.module.css';
 
 function SpaceCalendar() {
+  const [launchData, setLaunchData] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://ll.thespacedevs.com/2.0.0/launch/upcoming')
+      .then(response => setLaunchData(response.data.results))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
     <div className={SpaceCalendarCSS.SpaceCalendar}>
       <Navbar />
-      <h1>SpaceCalendar</h1>
+      {launchData.map(launch => (
+        <div key={launch.id}>
+          <h1>{launch.name}</h1>
+          <h3>{new Date(launch.net).toLocaleString('en-GB', { dateStyle: 'short', timeStyle: 'short' }).replace(',', ' |')}</h3>
+          <h3>{launch.status.name}</h3>
+          <h3>{launch.pad.location.name}</h3>
+          <h3>{launch.pad.name}</h3>
+          <h3>{launch.launch_service_provider.name}</h3>  
+        </div>
+      ))}
       <Footer />
     </div>
   );
