63de00156b224f72bf2eb2e397391d892a01e3fb
Domenico Cristo <84397096+domenicocristo@users.noreply.github.com>
2023-03-29 16:49:33 +0200

    Imported the Home and SpaceCalendar components links into the Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,19 @@
+import { Link } from 'react-router-dom';
 import React from 'react';
 import NavbarCSS from './Navbar.module.css';
 
 function Navbar() {
   return (
     <nav className={NavbarCSS.Navbar}>
-      <h3>My Site</h3>
+      <h1>Navbar</h1>
+      <ul className={NavbarCSS.Navbar__list}>
+        <li className={NavbarCSS.Navbar__item}>
+          <Link className={NavbarCSS.Navbar__link} to="/">Home</Link>
+        </li>
+        <li className={NavbarCSS.Navbar__item}>
+          <Link className={NavbarCSS.Navbar__link} to="/space-calendar">Space Calendar</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
